a2720137f85e1fbe719fc4df11fa2f1051e39a54
airtonmotoki <airton.motoki@gmail.com>
2021-06-30 18:38:16 -0300

    Create: base for ecs inspector

---
+++ Samples/ECSInspector/Scripts/UImGuiEntitiesWindow.cs
@@ -0,0 +1,26 @@
+﻿using ImGuiNET;
+using UImGuiManager.Core;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace UImGuiManager.Samples
+{
+	[CreateAssetMenu(fileName = "EntitiesWindow", menuName = "ImGui/SampleECSInspector/Entities Window", order = 1)]
+	public class UImGuiEntitiesWindow : UImGuiWindowBase<World>
+	{
+		public override void OnLayoutWindow()
+		{
+			var entityManager = Context.EntityManager;
+
+			var allEntities = entityManager.GetAllEntities(Allocator.Temp);
+
+			for (var index = 0; index < allEntities.Length; index++)
+			{
+				var entity = allEntities[index];
+				ImGui.Text(entityManager.GetName(entity));
+			}
+			allEntities.Dispose();
+		}
+	}
+}
---
+++ Samples/ECSInspector/Scripts/UImGuiMenuBarECSInspector.cs
@@ -0,0 +1,12 @@
+using UImGuiManager.Core;
+using Unity.Entities;
+using UnityEngine;
+
+namespace UImGuiManager.Samples
+{
+	[CreateAssetMenu(fileName = "MenuBarECSInspector", menuName = "ImGui/SampleECSInspector/MenuBarECSInspector", order = 1)]
+	public class UImGuiMenuBarECSInspector : UImGuiMenuBarBase<World>
+	{
+
+	}
+}
