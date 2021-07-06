0085e46d6c16cebb53fd566e854aabf75a3520c0
airtonmotoki <airton.motoki@gmail.com>
2021-07-06 17:04:39 -0300

    Create: entity list

--- Samples/ECSInspector/Scripts/UImGuiEntitiesWindow.cs
+++ Samples/ECSInspector/Scripts/UImGuiEntitiesWindow.cs
@@ -9,17 +9,56 @@ namespace UImGuiManager.Samples
 	[CreateAssetMenu(fileName = "EntitiesWindow", menuName = "ImGui/SampleECSInspector/Entities Window", order = 1)]
 	public class UImGuiEntitiesWindow : UImGuiWindowBase<World>
 	{
+		public static Entity SelectedEntity;
+
 		public override void OnLayoutWindow()
 		{
 			var entityManager = Context.EntityManager;
 
 			var allEntities = entityManager.GetAllEntities(Allocator.Temp);
 
-			for (var index = 0; index < allEntities.Length; index++)
+			ImGuiTableFlags flags = ImGuiTableFlags.Borders | ImGuiTableFlags.Sortable | ImGuiTableFlags.ScrollY | ImGuiTableFlags.Resizable | ImGuiTableFlags.Hideable;
+			if (ImGui.BeginTable("entities_list", 3, flags))
 			{
-				var entity = allEntities[index];
-				ImGui.Text(entityManager.GetName(entity));
+				ImGui.TableSetupColumn("Index", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.NoHide);
+				ImGui.TableSetupColumn("Version");
+				ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.NoHide);
+
+				ImGui.TableHeadersRow();
+
+				for (var index = 0; index < allEntities.Length; index++)
+				{
+					var entity = allEntities[index];
+
+					ImGui.PushID(entity.Index);
+					ImGui.TableNextRow();
+
+					if (ImGui.TableSetColumnIndex(0))
+					{
+						if (ImGui.Selectable(entity.Index.ToString(), (SelectedEntity == entity), ImGuiSelectableFlags.SpanAllColumns | ImGuiSelectableFlags.AllowItemOverlap))
+						{
+							SelectedEntity = entity;
+							UnityEngine.Debug.Log(entityManager.GetName(entity));
+						}
+					}
+
+					if (ImGui.TableSetColumnIndex(1))
+					{
+						ImGui.Text(entity.Version.ToString());
+					}
+
+					if (ImGui.TableSetColumnIndex(2))
+					{
+						ImGui.Text(entityManager.GetName(entity));
+					}
+
+					ImGui.PopID();
+
+				}
+
+				ImGui.EndTable();
 			}
+
 			allEntities.Dispose();
 		}
 	}
